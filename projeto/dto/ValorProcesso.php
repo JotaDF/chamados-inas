5491984737ecf3ebd9363df08af37f9a046806c7
JotaDF <j.wilson.df@gmail.com>
2024-06-18 11:00:18 -0300

    Create ValorProcesso.php

---
+++ projeto/dto/ValorProcesso.php
@@ -0,0 +1,13 @@
+<?php
+  class ValorProcesso{
+	    public $id;
+      public $id_tipo_valor;
+      public $id_processo;
+      public $valor;
+      public $cadastro;
+
+      //variaveis de manipulação
+      public $excluir;
+      public $status = true;
+      public $msg;	
+  }
