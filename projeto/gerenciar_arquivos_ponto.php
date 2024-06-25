e6a36855a012dfa1d0e1729fc3eb081b161ccb65
JotaDF <j.wilson.df@gmail.com>
2024-06-25 09:08:55 -0300

    Update gerenciar_arquivos_ponto.php

--- projeto/gerenciar_arquivos_ponto.php
+++ projeto/gerenciar_arquivos_ponto.php
@@ -142,7 +142,7 @@ and open the template in the editor.
 
                                     foreach ($files as $file) { ?>
                                         <div id='file-<?=$file ?>' class='col-xl-3 col-md-2 mb-4' style='max-width: 280px; max-height: 100px;'>
-                                                <a href="<?=$uploadDir . $file ?>"><img src="img/pdf_icon.svg" width="50" height="50" /></a> <?=str_replace(".pdf","",$file) ?> 
+                                                <a href="<?=$uploadDir . $file ?>" target="_blank"><img src="img/pdf_icon.svg" width="50" height="50" /></a> <?=str_replace(".pdf","",$file) ?> 
                                                 <a  href="javascript:void(0);" onclick="deleteFile('<?=$file ?>', this.parentNode)"><i class='far fa-trash-alt text-danger'></i></a>
                                         </div>
                                    <?php
