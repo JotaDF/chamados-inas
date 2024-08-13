12c04c7d9e76d8f19b4de56911288372a0baea3b
JotaDF <j.wilson.df@gmail.com>
2024-08-13 10:23:58 -0300

    Update save_nota_pagamento.php

--- projeto/save_nota_pagamento.php
+++ projeto/save_nota_pagamento.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('./actions/ManterNotaPagamento.php');
-require_once('./dto/OpcaoNotaPagamento.php');
+require_once('./dto/NotaPagamento.php');
 
 $db_nota_pagamento = new ManterNotaPagamento();
 $np = new NotaPagamento();
@@ -12,10 +12,10 @@ $np->pagamento       = $_POST['id_pagamento'];
 $np->numero             = $_POST['numero'];
 $np->valor              = $_POST['valor'];
 $np->exercicio          = $_POST['exercicio'];
-$np->data_emissao       = $_POST['data_emissao'];
-$np->data_validacao     = $_POST['data_validacao'];
+$np->data_emissao       = isset($_POST['data_emissao']) ? strtotime($_POST['data_emissao']) : '';
+$np->data_validacao     = isset($_POST['data_validacao']) ? strtotime($_POST['data_validacao']) : '';
 
-$db_nota_pagamento->save($np);
+$db_nota_pagamento->salvar($np);
 
 header('Location: gerenciar_pagamentos_prestador.php?id='.$id_prestador);
 
