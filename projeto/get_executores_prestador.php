fc300c4a8d03ce70fa9d671d537a97008e6e544f
JotaDF <j.wilson.df@gmail.com>
2024-08-29 12:01:04 -0300

    Update get_executores_prestador.php

--- projeto/get_executores_prestador.php
+++ projeto/get_executores_prestador.php
@@ -7,9 +7,9 @@
             echo "  <td>".($obj->editor > 0 ? 'Sim':'Não')."</td>";
             echo "  <td>".($obj->ativo > 0 ? 'Sim':'Não')."</td>";
             $btn_ativar = "";
-            $txt_status = "<a title='Ativar' href='mudar_status_executor.php?id_usuario=".$obj->id."&id_prestador=".$prestador->id."&status=1' ><img src='./img/nao_visivel.svg' width='30'/></a>";
+            $txt_status = "<a title='Ativar' href='mudar_status_executor.php?id_usuario=".$obj->id."&id_prestador=".$prestador->id."&status=1' class='btn btn-success btn-sm'><i class='fa fa-eye'></i></a>";
             if($obj->ativo == 1){
-                $txt_status = "<a title='Desativar' href='mudar_status_executor.php?id_usuario=".$obj->id."&id_prestador=".$prestador->id."&status=0' ><img src='./img/visivel.svg' width='30'/></a>";
+                $txt_status = "<a title='Desativar' href='mudar_status_executor.php?id_usuario=".$obj->id."&id_prestador=".$prestador->id."&status=0' class='btn btn-warning btn-sm' ><i class='fa fa-eye-slash'></i></a>";
             }
             if($usuario_logado->perfil <= 2 || $obj->editor == 1){
                 if($usuario_logado->perfil <= 2){
