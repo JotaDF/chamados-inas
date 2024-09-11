b87a7104539b4babd0c47310cc5b957e8368b8b3
kakabraga <cauebraga96@outlook.com>
2024-09-11 11:56:47 -0300

    Create CartaRecurso.php

---
+++ projeto/dto/CartaRecurso.php
@@ -0,0 +1,14 @@
+<?php
+
+Class CartaRecurso extends Model {
+    public $id;
+    public $carta_informativo;
+    public $exercicio;
+    public $valor_deferido;
+    public $id_nota_glosa;
+    public $data_atesto;
+    public $data_pagamento;
+
+    public $excluir;
+    public $msg;
+}
