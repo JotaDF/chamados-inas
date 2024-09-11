93826ed2f4698f0f7f4371b7fd3315a457d41f7e
kakabraga <cauebraga96@outlook.com>
2024-09-11 11:51:07 -0300

    Create NotaGlosa.php

---
+++ projeto/dto/NotaGlosa.php
@@ -0,0 +1,16 @@
+<?php 
+
+Class NotaGlosa {
+    public $id;
+    public $numero;
+    public $lote;
+    public $valor;
+    public $id_recurso_glosa;
+    public $exercicio;
+    public $data_emissao; 
+    public $data_validacao; 
+    public $excluir;
+    public $msg;
+
+
+}
