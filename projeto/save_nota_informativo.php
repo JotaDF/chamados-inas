cacf436919277e3907fd36bd2b7489367cba2b9f
kakabraga <cauebraga96@outlook.com>
2024-09-19 12:38:46 -0300

    Create save_nota_informativo.php

---
+++ projeto/save_nota_informativo.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('./actions/ManterCartaRecurso.php');
+require_once('./dto/CartaRecurso.php');
+
+require_once('./actions/ManterAuditoria.php');
+require_once('./dto/Auditoria.php');
+
+$db_auditoria = new ManterAuditoria();
+$db_carta_recurso = new ManterCartaRecurso();
+$cr = new CartaRecurso();
+
+
+$id_usuario                    = $_REQUEST['id_usuario'];
+$id_prestador                  = $_POST['id_prestador'];
+
+$cr->id_nota_glosa             = $_POST['id_nota_glosa'];
+$cr->carta_informativo         = $_POST['carta_informativo'];
+$cr->exercicio                 = $_POST['exercicio'];
+$cr->valor_deferido            = $_POST['valor_deferido'];
+$db_carta_recurso->salvar($cr);
+
+
+
+
+header('Location: gerenciar_glosas_prestador.php?id='.$id_prestador);
+
