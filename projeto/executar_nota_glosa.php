ab627d61dfa992fe2d35fed98175ccde957b4fa7
JotaDF <j.wilson.df@gmail.com>
2024-09-23 18:04:15 -0300

    Create executar_nota_glosa.php

---
+++ projeto/executar_nota_glosa.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('./actions/ManterNotaGlosa.php');
+require_once('./actions/ManterAuditoria.php');
+require_once('./dto/Auditoria.php');
+
+$db_nota_glosa = new ManterNotaGlosa();
+$db_auditoria = new ManterAuditoria();
+
+$id_usuario = $_REQUEST['id_usuario'];
+$id = $_REQUEST['id'];
+$id_prestador = $_REQUEST['id_prestador'];
+if ($id > 0) {
+    $db_nota_glosa->executar($id);
+    //Auditando processo
+    $a = new Auditoria();
+    $a->acao = "Executar Nota!";
+    $a->objeto = "NotaGlosa";
+    $a->informacao = "id_prestador= " . $id_prestador . " id_nota_glosa= " . $id;
+    $a->autor = $id_usuario;
+    $db_auditoria->salvar($a);
+
+    header('Location: gerenciar_glosas_prestador.php?id='.$id_prestador);
+} else {
+    echo 'Falta de parâmetro!';
+    header('Location: gerenciar_glosas_prestador.php?id='.$id_prestador);
+}
