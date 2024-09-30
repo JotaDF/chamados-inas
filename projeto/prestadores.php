fa1d83cdd81cea5bb827a7f93e48beba97c51272
JotaDF <j.wilson.df@gmail.com>
2024-09-30 18:04:39 -0300

    Update prestadores.php

--- projeto/prestadores.php
+++ projeto/prestadores.php
@@ -133,7 +133,7 @@ foreach ($listaS as $obj) {
                     <div class="container-fluid">
                         <?php include './form_prestador.php'; ?>
                         <!-- Project Card Example -->
-                        <div class="card mb-4 border-primary" style="max-width:1200px">
+                        <div class="card mb-4 border-primary" style="max-width:1300px">
                             <div class="row ml-0 card-header py-2 bg-gradient-primary" style="width:100%">
                                 <div class="col-sm ml-0" style="max-width:50px;">
                                     <i class="fa fa-th-large fa-2x text-white"></i> 
