a4afa5da91565632156969c12f2ab55274a675f2
JotaDF <j.wilson.df@gmail.com>
2024-09-30 15:10:12 -0300

    Create atestar_notas_lote_prestador.php

---
+++ projeto/atestar_notas_lote_prestador.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('./actions/ManterNotaPagamento.php');
+require_once('./actions/ManterNotaGlosa.php');
+
+$db_nota_pagamento = new ManterNotaPagamento();
+$db_nota_glosa = new ManterNotaGlosa();
+
+$dados = $_REQUEST['atesto'];
+$id_prestador = $_REQUEST['id_prestador'];
+
+$dados[] = $_REQUEST['atesto'];
+
+foreach($dados as $dado){
+    $reg = explode("#", $dado);
+    if ($reg[0] == "np") {
+        $db_nota_pagamento->atestar($reg[1]);
+    } else if ($reg[0] == "ng") {
+        $db_nota_glosa->atestar($reg[1]);
+    }
+}
+header('Location: painel_atestos_pendentes.php');
+
