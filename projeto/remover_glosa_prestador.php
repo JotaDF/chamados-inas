eb9d01229cb2b0ecaba780bb9c53038a220def05
JotaDF <j.wilson.df@gmail.com>
2024-10-02 11:38:13 -0300

    Update remover_glosa_prestador.php

--- projeto/remover_glosa_prestador.php
+++ projeto/remover_glosa_prestador.php
@@ -1,27 +1,27 @@
 <?php
 
-require_once('./actions/ManterGlosa.php');
+require_once('./actions/ManterCartaRecursada.php');
 require_once('./actions/ManterAuditoria.php');
 require_once('./dto/Auditoria.php');
 
-$db_glosa = new ManterGlosa();
+$db_carta_recursada = new ManterCartaRecursada();
 $db_auditoria = new ManterAuditoria();
 
 $id_usuario = $_REQUEST['id_usuario'];
 $id = $_REQUEST['id'];
 $id_prestador = $_REQUEST['id_prestador'];
 if ($id > 0) {
-    $db_glosa->excluir($id);
+    $db_carta_recursada->excluir($id);
     //Auditando processo
     $a = new Auditoria();
-    $a->acao = "Excluir Glosa!";
-    $a->objeto = "Glosa";
-    $a->informacao = "id_prestador= " . $id_prestador . " id_glosa= " . $id;
+    $a->acao = "Excluir CartaRecursada!";
+    $a->objeto = "CartaRecursada";
+    $a->informacao = "id_prestador= " . $id_prestador . " id_carta_recursada= " . $id;
     $a->autor = $id_usuario;
     $db_auditoria->salvar($a);
 
-    header('Location: gerenciar_glosas_prestador.php?id='.$id_prestador);
+    header('Location: gerenciar_carta_recursadas_prestador.php?id='.$id_prestador);
 } else {
     echo 'Falta de parâmetro!';
-    header('Location: gerenciar_glosas_prestador.php?id='.$id_prestador);
+    header('Location: gerenciar_carta_recursadas_prestador.php?id='.$id_prestador);
 }
