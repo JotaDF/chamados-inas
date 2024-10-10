e8ed2319020d378ddb6d94ef7378e33a1c1a5819
JotaDF <j.wilson.df@gmail.com>
2024-10-10 14:42:09 -0300

    resolver ocorrencia

--- projeto/get_ocorrencias_nota.php
+++ projeto/get_ocorrencias_nota.php
@@ -7,9 +7,9 @@
             echo "  <td>".($obj->resolvido > 0 ? 'Sim':'Não')."</td>";
             echo "  <td>".date('d/m/Y h:i', strtotime($obj->data))."</td>";
             $btn_resolver = "";
-            $txt_status = "<a title='Marcar como resolvida!' href='mudar_status_ocorrencia_nota.php?id_nota=".$obj->id."&id_prestador=".$prestador->id."&status=1' class='btn btn-success btn-sm'><i class='fa fa-check'></i></a>";
+            $txt_status = "<a title='Marcar como resolvida!' href='mudar_status_ocorrencia_nota.php?id_nota=".$obj->id."&id_prestador=".$prestador->id."&tp=".$tp."&status=1' class='btn btn-success btn-sm'><i class='fa fa-check'></i></a>";
             if($obj->ativo == 1){
-                $txt_status = "<a title='Marcar como não resolvida!' href='mudar_status_executor.php?id_usuario=".$obj->id."&id_prestador=".$prestador->id."&status=0' class='btn btn-warning btn-sm' ><i class='fa fa-exchange'></i></a>";
+                $txt_status = "<a title='Marcar como não resolvida!' href='mudar_status_ocorrencia_nota.php?id_nota=".$obj->id."&id_prestador=".$prestador->id."&tp=".$tp."&status=0' class='btn btn-warning btn-sm' ><i class='fa fa-exchange'></i></a>";
             }
             $btn_alterar = "<button title='Excluir' class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",".$usuario_logado->id.",\"".$obj->descricao."\",".$prestador->id.",".$id_nota.")'><i class='fas fa-edit'></i></button>";
             if($usuario_logado->perfil <= 2){
