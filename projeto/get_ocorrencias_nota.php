d3f78bc05c46087e494128cffe6c65bf38182d3d
JotaDF <j.wilson.df@gmail.com>
2024-10-11 14:33:32 -0300

    Update get_ocorrencias_nota.php

--- projeto/get_ocorrencias_nota.php
+++ projeto/get_ocorrencias_nota.php
@@ -8,21 +8,15 @@
             echo "  <td>".date('d/m/Y h:i', strtotime($obj->data))."</td>";
             $btn_resolver = "";
             $txt_status = "<a title='Marcar como resolvida!' href='mudar_status_ocorrencia_nota.php?id=".$obj->id."&id_nota=".$id_nota."&id_prestador=".$prestador->id."&tp=".$tp."&status=1' class='btn btn-success btn-sm'><i class='fa fa-check'></i></a>";
-            if($obj->ativo == 1){
-                $txt_status = "<a title='Marcar como não resolvida!' href='mudar_status_ocorrencia_nota.php?id=".$obj->id."&id_nota=".$id_nota."&id_prestador=".$prestador->id."&tp=".$tp."&status=0' class='btn btn-warning btn-sm' ><i class='fa fa-exchange'></i></a>";
+            if($obj->resolvido == 1){
+                $txt_status = "<a title='Marcar como não resolvida!' href='mudar_status_ocorrencia_nota.php?id=".$obj->id."&id_nota=".$id_nota."&id_prestador=".$prestador->id."&tp=".$tp."&status=0' class='btn btn-warning btn-sm' ><i class='fa fa-random'></i></a>";
             }
             $btn_alterar = "<button title='Excluir' class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",".$usuario_logado->id.",\"".$obj->descricao."\",".$prestador->id.",".$id_nota.")'><i class='fas fa-edit'></i></button>";
-            if($usuario_logado->perfil <= 2){
-                if($usuario_logado->perfil <= 2){
-                    $btn_resolver =  "&nbsp;&nbsp;" . $txt_status;
-                }
-                if($obj->resolvido == 0){
-                    echo "  <td align='center'>".$btn_alterar."&nbsp;&nbsp;<button title='Excluir' class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",".$usuario_logado->id.",\"".$obj->descricao."\",".$prestador->id.",".$id_nota.")'><i class='far fa-trash-alt'></i></button>".$btn_resolver."</td>";
-                } else {
-                    echo "  <td align='center'>".$btn_alterar."&nbsp;&nbsp;<button title='Ocorrência já resolvida' class='btn btn-secondary btn-sm' type='button'><i class='far fa-trash-alt'></i></button>".$btn_ativar."</td>";
-                }
+            $btn_resolver =  "&nbsp;&nbsp;" . $txt_status;
+            if($obj->resolvido == 0){
+                echo "  <td align='center'>".$btn_alterar."&nbsp;&nbsp;<button title='Excluir' class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",".$usuario_logado->id.",\"".$obj->descricao."\",".$prestador->id.",".$id_nota.")'><i class='far fa-trash-alt'></i></button>".$btn_resolver."</td>";
             } else {
-                echo "  <td align='center'> - </td>";                
+                echo "  <td align='center'>".$btn_alterar."&nbsp;&nbsp;<button title='Ocorrência já resolvida' class='btn btn-secondary btn-sm' type='button'><i class='far fa-trash-alt'></i></button>".$btn_resolver."</td>";
             }
             echo "</tr>";
         }
