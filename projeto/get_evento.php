6a8b38e3343e368cbfbb60a2756052c32b0a16b3
JotaDF <j.wilson.df@gmail.com>
2024-12-05 15:40:11 -0300

    Update get_evento.php

--- projeto/get_evento.php
+++ projeto/get_evento.php
@@ -17,12 +17,13 @@
                 $txt_status = "<a href='mudar_status_evento.php?id=".$obj->id."&status=0' ><img src='./img/visivel.svg' width='30'/></a>";
             }
             echo "  <td align='center'>".$txt_status."</td>";
-            $btn_inscricoes = '&nbsp;&nbsp;<a href="gerenciar_inscricoes_evento.php?id='.$obj->id.'" title="Gerenciar opções" class="btn btn-warning btn-sm" type="button"><i class="fa fa-list-ul"></i></a>';
+            $btn_folder= '&nbsp;&nbsp;<a href="gerenciar_folder_evento.php?id='.$obj->id.'" title="Gerenciar folder" class="btn btn-warning btn-sm" type="button"><i class="fa fa-file-pdf"></i></a>';
+            $btn_inscricoes = '&nbsp;&nbsp;<a href="gerenciar_inscricoes_evento.php?id='.$obj->id.'" title=":Lista de inscritos" class="btn btn-warning btn-sm" type="button"><i class="fa fa-list-ul"></i></a>';
             $btn_alterar = "<button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->titulo."\",\"".$obj->descricao."\",\"".$obj->inscreve."\",\"".$obj->data."\",\"".$obj->hora."\")'><i class='fas fa-edit'></i></button>";
             if($obj->excluir){
-                echo "  <td align='center'>" . $btn_alterar . "&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->descricao."\")'><i class='far fa-trash-alt'></i></button>" . $btn_inscricoes . "</td>";
+                echo "  <td align='center'>" . $btn_alterar . "&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->descricao."\")'><i class='far fa-trash-alt'></i></button>" . $btn_inscricoes . $btn_folder .  "</td>";
             } else {
-                echo "  <td align='center'>" . $btn_alterar . "&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button>" . $btn_inscricoes  . "</td>";                
+                echo "  <td align='center'>" . $btn_alterar . "&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button>" . $btn_inscricoes . $btn_folder ."</td>";                
             }
             echo "</tr>";
         }
