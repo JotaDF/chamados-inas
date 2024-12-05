05c7cece56ffd62e39161a2e0a6e11fd05580502
JotaDF <j.wilson.df@gmail.com>
2024-12-05 17:08:53 -0300

    Update get_folder_evento.php

--- projeto/get_folder_evento.php
+++ projeto/get_folder_evento.php
@@ -1,19 +1,16 @@
 <?php
 
-$numero = $_REQUEST['numero'];
-$ano = $_REQUEST['ano'];
+$id = $_REQUEST['id'];
 
-$uploadDir = 'contratos/';
-$uploadDir .= $numero;
-$uploadDir .= '_';
-$uploadDir .= $ano;
+$uploadDir = 'eventos/folder_';
+$uploadDir .= $id;
 $uploadDir .= '/';
                            
 $files = array_diff(scandir($uploadDir), array('.', '..'));
 
 foreach ($files as $file) { ?>
     <div id='file-<?=$file ?>' class='col-xl-3 col-md-2 mb-4' style='max-width: 280px; max-height: 100px;'>
-            <a href="<?=$uploadDir . $file ?>" target="_blank"><img src="img/pdf_icon.svg" width="50" height="50" /></a> <?=str_replace(".pdf","",$file) ?> 
+            <img src="<?=$uploadDir . $file ?>" height="200" width="200"> 
             <a  href="javascript:void(0);" onclick="excluir('<?=$file ?>')"><i class='far fa-trash-alt text-danger'></i></a>
     </div>
 <?php
