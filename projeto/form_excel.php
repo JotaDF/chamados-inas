b1e687637de04dec84a69caf9526bdad7704ffbe
kakabraga <cauebraga96@outlook.com>
2024-12-05 12:06:23 -0300

    Update conversor xml

---
+++ projeto/form_excel.php
@@ -0,0 +1,67 @@
+<form action="xml_3.php" method="POST" enctype="multipart/form-data">
+    <div class="container">
+        <div class="card-body ">
+            <div class="row g-3">
+                <div class="col">
+                    <label for="formFile" class="form-label" >Anexar Arquivo Excel</label>
+                    <input class="form-control" type="file" name="excel" id="excel" accept=".xlsx, .xls, .xlt"
+                        required>
+                </div>
+                <div class="col">
+                    <label for="retificacao1" class="form-label">Retificação</label>
+                    <select class="form-select" name="reti" id="retificacao1" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+
+            </div>
+            <div class="row g-3">
+                <div class="col">
+                    <label for="retificacao3" class="form-label">TPMB</label>
+                    <select class="form-select" name="tpamb" id="retificacao3" aria-label="Default select example">
+                        <option selected></option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <div class="mb-2">
+                        <label for="retificacao2" class="form-label">ProcEmi</label>
+                        <select class="form-select" name="procEmi" id="retificacao2" aria-label="Default select example">
+                            <option selected></option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="mb-3">
+                        <label for="retificacao2" class="form-label">Natureza do Rendimento</label>
+                        <select class="form-select" name="rend" id="retificacao2" aria-label="Default select example">
+                            <option selected></option>
+                            <option value="1">1705</option>
+                        </select>
+                    </div>
+
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Periodo de Apuração</label>
+                <input type="date" class="form-control" name="data">
+            </div>
+            <div class="mb-3">
+                <button type="submit" name="envio" class="btn btn-primary btn-sm">Converter XML</button>&nbsp&nbsp
+                <button type="submit" name="baixar" class="btn btn-secondary btn-sm">
+                   Baixar XML&nbsp&nbsp&nbsp<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 3.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5M8 6a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7.5 12.293V6.5A.5.5 0 0 1 8 6"/>
+</svg></button>
+            </div>
+        </div>
+</form>
+
+</div>
+
+
+</div>
---
+++ projeto/form_excel_xml.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <title>Leitor de Excel</title>
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" />
+    <!------ Include the above in your HEAD tag ---------->
+
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css">
+
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" language="javascript"
+        src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" language="javascript"
+        src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script type="text/javascript" language="javascript"
+        src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+    <style>
+        .centralizado {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            /* Ajuste conforme necessário */
+        }
+    </style>
+
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        <?php include './menu_gerente.php'; ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php //include './top_bar.php'; ?>
+                <div class="conainer-fluid">
+                    <div class="card mb-4 border-primary" style="max-width:1000px">
+                        <div class="row ml-0 card-header py-2 bg-gradient-primary" style="width:100%">
+                            <div class="col-sm ml-0 text-right" style="max-width:50px;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                                    class="bi bi-filetype-xml text-white"
+                                    viewBox="0 0 16 16">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                    <path fill-rule="evenodd"
+                                        d="M14 4.5V14a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM3.527 11.85h-.893l-.823 1.439h-.036L.943 11.85H.012l1.227 1.983L0 15.85h.861l.853-1.415h.035l.85 1.415h.908l-1.254-1.992zm.954 3.999v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596h-.025L4.58 11.85h-.806v3.999zm4.71-.674h1.696v.674H8.4V11.85h.791z" />
+                                </svg>
+                            </div>
+                            <div class="col mb-0">
+                                <span style="align:left;" class="h5 m-0 font-weight text-white">Conversor para
+                                    XML</span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ">
+                            <div class="row ">
+                                <div class="card-body ">
+                                    <?php include './form_excel.php'; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php include './rodape.php'; ?>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+</body>
+
+</html>
