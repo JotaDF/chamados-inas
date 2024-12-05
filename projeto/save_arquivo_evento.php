d845680a069bce9aa8749798a85986fac98f453e
JotaDF <j.wilson.df@gmail.com>
2024-12-05 17:37:31 -0300

    Update save_arquivo_evento.php

--- projeto/save_arquivo_evento.php
+++ projeto/save_arquivo_evento.php
@@ -1,11 +1,12 @@
 <?php
 
-$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : "";
+$id = $_REQUEST['id'];
 
 if(!empty($_FILES['file'])){
     $caminho = "eventos/folder_" . $id . "/";
     $arquivo = basename($_FILES['file']['name']);
     $targetFilePath = $caminho . $arquivo;
+    echo $targetFilePath;
     if(move_uploaded_file($_FILES['file']['tmp_name'], $targetFilePath)){
         echo 'Arquivo enviado';
     }
