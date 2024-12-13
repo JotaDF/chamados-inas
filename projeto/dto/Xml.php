7a8391a39182a45452bc8ee6706fdcce0a962543
kakabraga <cauebraga96@outlook.com>
2024-12-13 12:15:20 -0300

    Create Xml.php

---
+++ projeto/dto/Xml.php
@@ -0,0 +1,27 @@
+<?php
+
+class Xml
+{
+    public $id;
+    public $evtRetPJ;
+    public $indRetif;
+    public $perApur;
+    public $tpAmb;
+    public $reti;
+    public $data;
+    public $netRend;
+    public $procEmi;
+    public $verProc;
+    public $tpInsc;
+    public $nrInsc;
+    public $tpInscEstab;
+    public $nrInscEstab;
+    public $cnpj_benef;
+    public $natRend;
+    public $nota;
+    public $data_pagamento;
+    public $processo;
+    public $valor_calculo_imposto;
+    public $retencao;
+    public $file;
+}
