c99b3580b3ba46d7801e68d9a62f528eebe9b39f
kakabraga <cauebraga96@outlook.com>
2024-12-13 12:18:30 -0300

    Update Xml

--- projeto/gerador_xml.php
+++ projeto/gerador_xml.php
@@ -1,4 +1,5 @@
 <?php
+require './actions/ManterXml.php';
 require 'vendor/phpspreadsheet/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
@@ -14,8 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['excel'])) {
         $spreadsheet = IOFactory::load($file);
         $worksheet = $spreadsheet->getActiveSheet();
 
-        function cleanTagName($name)
-        {
+        function cleanTagName($name) {
             $name = preg_replace('/[^a-zA-Z0-9_]/', '_', trim($name));
             $name = preg_replace('/_{2,}/', '_', $name);
             return $name;
@@ -121,93 +121,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['excel'])) {
                 $idePgto->appendChild($infoPgto);
             }
         }
-
-        $xml = simplexml_load_file('arquivos_xml/saida.xml');
-
-        $namespace = 'http://www.reinf.esocial.gov.br/schemas/evt4020PagtoBeneficiarioPJ/v2_01_02';
-        $xml->registerXPathNamespace('ns', $namespace);
-        // colocar sua conexão aqui 
-        $conn = new mysqli('localhost', 'root', '.inas@2023', 'inas');
-
-
-        if ($conn->connect_error) {
-            die("Falha na conexão: " . $conn->connect_error);
-        }
-
-        foreach ($xml->xpath('//ns:evtRetPJ') as $evtRetPJ) {
-            $id = (string) $evtRetPJ['id'];
-        }
-
-        foreach ($xml->xpath('//ns:ideEvento') as $ideEvento) {
-            $indRetif = (int) $ideEvento->indRetif;
-            $perApur = (string) $ideEvento->perApur;
-            $tpAmb = (int) $ideEvento->tpAmb;
-            $procEmi = (int) $ideEvento->procEmi;
-            $verProc = (string) $ideEvento->verProc;
-        }
-
-        foreach ($xml->xpath('//ns:ideContri') as $ideContri) {
-            $tpInsc = (int) $ideContri->tpInsc;
-            $nrInsc = (int) $ideContri->nrInsc;
-        }
-
-        foreach ($xml->xpath('//ns:infoPgto') as $infoPgto) {
-            $dataPagamento = (string) $infoPgto->DATA_PAGAMENTO;
-            $processo = (string) $infoPgto->PROCESSO;
-            $valorCalculoImpostos = (float) $infoPgto->VALOR_B_CALCULO_IMPOSTOS;
-            $nota = (string) $infoPgto->NOTA_FISCAL;
-            $retencoes = (float) $infoPgto->retencoes->RETENCAO_IRRF;
-            $cnpj = (string) $infoPgto->CNPJ;
-
-            if (is_numeric($dataPagamento)) {
-                $dataPagamentoTimestamp = strtotime("1900-01-01 +$dataPagamento days -2 days");
-                $dataPagamentoFormatada = date("Y-m-d", $dataPagamentoTimestamp);
-            } else {
-                $dataPagamentoFormatada = $dataPagamento;
-            }
-
-            $valorRetencaoIRRF = 0;
-
-            foreach ($xml->xpath('//ns:ideBenef') as $ideBenef) {
-                $cnpjBenf = (string) $ideBenef->cnpjBenef;
-                $natRend = (string) $ideBenef->idePgto->natRend;
-
-                foreach ($xml->xpath('//ns:ideEstab') as $ideEstab) {
-                    $tipo_insc_estab = (string) $ideEstab->tpInscEstab;
-                    $numero_insc_estab = (string) $ideEstab->nrInscEstab;
-                }
-            }
-
-            $stmt = $conn->prepare("INSERT INTO XML (data_pagamento, processo, valor_calculo_imposto, cnpj_benef, natRend, nota, nrInscEstab, tpInscEstab, retencao, tpInsc, nrInsc, indRetif, perApur, tpAmb, procEmi, verProc, evtRetPJ) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-            if ($stmt === false) {
-                die("Erro na preparação da declaração: " . $conn->error);
-            }
-
-            $stmt->bind_param('ssdsssssdssssiiss', $dataPagamento, $processo, $valorCalculoImpostos, $cnpj, $natRend, $nota, $numero_insc_estab, $tipo_insc_estab, $retencoes, $tpInsc, $nrInsc, $indRetif, $perApur, $tpAmb, $procEmi, $verProc, $id);
-
-            if (!$stmt->execute()) {
-                echo "Erro ao inserir: " . $stmt->error . "\n";
-            }
-        }
-
         if (isset($_POST['baixar'])) {
             header('Content-Type: application/xml');
             header('Content-Disposition: attachment; filename="saida.xsd"');
+            $xml = new ManterXml();
+            $path = 'arquivos_xml/saida.xml';
+            $xml->salvaXml($path);
             echo $dom->saveXML();
             exit;
         }
-        if ($stmt) {
-            header('Location: gerenciar_xml.php');
-            $stmt->close();
-            $conn->close();
-            echo "Dados inseridos com sucesso!";
-        }
-
-
         $dom->save('arquivos_xml/saida.xml');
-        header('Location: gerenciar_xml.php');
+        header('Location:   ManterXml.php');
         exit;
     } catch (Exception $e) {
         echo 'Erro ao processar o arquivo: ' . $e->getMessage();
