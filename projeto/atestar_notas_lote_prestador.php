2aee220006622c152440fcfba35c334e4d0a9332
JotaDF <j.wilson.df@gmail.com>
2025-01-23 09:10:00 -0300

    Update atestar_notas_lote_prestador.php

--- projeto/atestar_notas_lote_prestador.php
+++ projeto/atestar_notas_lote_prestador.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once('./actions/ManterNotaPagamento.php');
-require_once('./actions/ManterNotaGlosa.php');
+require_once('./actions/ManterCartaRecurso.php');
 require_once('./actions/ManterAuditoria.php');
 require_once('./dto/Auditoria.php');
 
 $db_nota_pagamento = new ManterNotaPagamento();
-$db_nota_glosa = new ManterNotaGlosa();
+$db_nota_glosa = new ManterCartaRecurso();
 $db_auditoria = new ManterAuditoria();
 
 $dados = $_REQUEST['atesto'];
@@ -31,8 +31,8 @@ foreach($dados as $dado){
         //Auditando processo
         $a = new Auditoria();
         $a->acao = "Atestar Nota de glosa em lote!";
-        $a->objeto = "NotaGlosa";
-        $a->informacao = "id_nota_glosa= " . $reg[1];
+        $a->objeto = "CartaRecurso";
+        $a->informacao = "id_carta_recurso= " . $reg[1];
         $a->autor = $reg[2];  //id usuario logado
         $db_auditoria->salvar($a);
     }
