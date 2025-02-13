5a790e728084fadfaa1e57b384df4f811b6b2a84
kakabraga <cauebraga96@outlook.com>
2025-02-13 11:38:30 -0300

    Update dashboard_bar.php

--- projeto/dashboard_bar.php
+++ projeto/dashboard_bar.php
@@ -29,13 +29,13 @@
             });
         });
 
-        // Função para atualizar o gráfico
+        
         function atualizarGrafico(dados, ano) {
             var ctx = document.getElementById('meuGrafico').getContext('2d');
 
-            // Verifica se o gráfico já foi criado e, se sim, destrói ele antes de criar um novo
+            
             if (window.meuGrafico instanceof Chart) {
-                window.meuGrafico.destroy(); // Destrói o gráfico existente
+                window.meuGrafico.destroy(); 
             }
 
             // Definindo os meses manualmente
@@ -46,26 +46,23 @@
             ];
 
             // Ternário para verificar se o ano for 'todos', usar os anos. Se não, usamos os meses.
-            var labels = ano === 'todos' ? meses : meses;
+            var labels = meses;
 
-            // Definindo as cores para cada ano
+            
             const coresPorAno = {
                 '2021': { backgroundColor: 'rgba(60, 120, 216, 0.3)', borderColor: 'rgba(60, 120, 216, 1)' },
                 '2022': { backgroundColor: 'rgba(0, 204, 153, 0.3)', borderColor: 'rgba(0, 204, 153, 1)' },
                 '2023': { backgroundColor: 'rgba(255, 133, 27, 0.3)', borderColor: 'rgba(255, 133, 27, 1)' },
                 '2024': { backgroundColor: 'rgba(156, 89, 210, 0.3)', borderColor: 'rgba(156, 89, 210, 1)' },
-                'todos': { backgroundColor: 'rgba(0, 172, 210, 0.7)', borderColor: 'rgba(0, 172, 210, 1)' }
             };
 
             // Preparar os dados para o gráfico
             var datasets = [];
-
-            // Se o ano for 'todos', criamos um dataset para cada ano
             if (ano === 'todos') {
                 for (let anoItem in dados) {
                     datasets.push({
                         label: anoItem,
-                        data: Object.values(dados[anoItem]), // Pega os valores dos meses
+                        data: Object.values(dados[anoItem]), 
                         backgroundColor: coresPorAno[anoItem].backgroundColor,
                         borderColor: coresPorAno[anoItem].borderColor,
                         borderWidth: 1
@@ -75,7 +72,7 @@
                 // Caso contrário, para um único ano, preenche os dados para os meses
                 datasets.push({
                     label: ano,
-                    data: Object.values(dados), // Pega os valores dos meses
+                    data: Object.values(dados), 
                     backgroundColor: coresPorAno[ano].backgroundColor,
                     borderColor: coresPorAno[ano].borderColor,
                     borderWidth: 1
