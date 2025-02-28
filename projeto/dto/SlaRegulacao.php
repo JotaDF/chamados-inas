a36773da87a205d9cd22064514f56cfd99238341
kakabraga <cauebraga96@outlook.com>
2025-02-28 08:48:37 -0300

    Create SlaRegulacao.php

---
+++ projeto/dto/SlaRegulacao.php
@@ -0,0 +1,12 @@
+<?php 
+
+ Class SlaRegulacao {
+    public $autorizacao;
+    public $tipo_guia;
+    public $area;
+    public $fila;
+    public $encaminhamento_manual;
+    public $data_solicitacao_t;
+    public $data_solicitacao_d;
+
+ }
