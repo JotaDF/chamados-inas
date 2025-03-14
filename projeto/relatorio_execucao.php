5d7bd302274d2b696147830687ae461694d81891
kakabraga <cauebraga96@outlook.com>
2025-03-14 13:08:30 -0300

    Create Relatorio Execução

---
+++ projeto/relatorio_execucao.php
@@ -0,0 +1,219 @@
+<?php
+$mod = 10;
+require_once('./verifica_login.php');
+?> 
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>GERENTE - Gerenciador de atendimentos</title>
+
+        <!-- Custom fonts for this template-->
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+        <!-- Custom styles for this template-->
+        <link href="css/sb-admin-2.min.css" rel="stylesheet">
+        <link rel="shortcut icon" href="favicon.ico" />
+        <!------ Include the above in your HEAD tag ---------->
+
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css">
+
+        <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+
+        <script src="https://cdn.jsdelivr.net/gh/linways/table-to-excel@v1.0.4/dist/tableToExcel.js"></script>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.4.1/jspdf.debug.js" integrity="sha384-THVO/sM0mFD9h7dfSndI6TS0PgAGavwKvB5hAxRRvc0o9cPLohB0wb/PTA7LdUHs" crossorigin="anonymous"></script>
+
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+
+
+        <script src="https://rawgit.com/eKoopmans/html2pdf/master/dist/html2pdf.bundle.min.js"></script>
+
+        <style>
+            body{
+                font-size: small;
+            }
+            .nao-ver{
+                display:none;
+            }
+        </style>
+        <script type="text/javascript" class="init">
+            function naover(classe) {
+                $("." + classe).css("display", "none");
+                //$('#btn_editar').toggleClass('fa-lock fa-unlock');
+            }
+            $(document).ready(function () {
+                $("#btnExport").click(function () {
+                    let table = $("#registros");
+                    TableToExcel.convert(table[0], {// html code may contain multiple tables so here we are refering to 1st table tag
+                        name: 'relatorio_execucao.xlsx', // fileName you could use any name
+                        sheet: {
+                            name: 'Sheet 1' // sheetName
+                        }
+                    });
+                });
+
+                $("#btnPDF").click(function () {
+                    savePDF();
+                });
+
+            });
+
+        </script>
+    </head>
+
+    <body id="page-top">
+
+        <?php
+        include_once('actions/ManterCartaRecursada.php');
+        include_once('actions/ManterCartaRecurso.php');
+        include_once('actions/ManterNotaGlosa.php');
+        include_once('actions/ManterNotaPagamento.php');
+
+        $mNotaGlosa = new ManterNotaGlosa();
+        $mCartaRecurso = new ManterCartaRecurso();
+        $mCartaRecursada = new ManterCartaRecursada();
+        $mNotaPagamento= new ManterNotaPagamento();
+
+        $filtro = isset($_POST['filtro']) ? $_POST['filtro'] : '';
+        $exercicio = isset($_POST['exercicio']) ? $_POST['exercicio'] : " ";
+        $nota_glosa = array();
+        $nota_pagamento = array();
+        if ($filtro == 'nota_glosa') {
+            $nota_glosa = $mCartaRecurso->listarCartaPorExercicio($exercicio);
+        } else if ($filtro == 'nota_pagamento') {
+            $nota_pagamento = $mNotaPagamento->listarNotaPorExercicio($exercicio);
+        } else if ($filtro == 'todos') {
+            //chamar função
+        }
+       
+        ?>
+        <!-- Begin Page Content -->
+        <!-- Content Row -->
+        
+        <div id="containerNotaGlosa" class="container-fluid align-items-center" style="width:95%">
+            <?php
+            if ($nota_glosa && count($nota_glosa) > 0) {
+                ?>
+
+            <div id="containerNotaGlosa" role="main" class="align-items-center" style="width:100%"><h2 class="text-center">Relatório da Execução: Nota Glosa</h2><img src="img/iconexcel.png" width="30" height="30" class="d-print-none" id="btnExport" />
+                    <table class="table table-striped" id="registros">
+                    <tr class="thead-dark">
+                            <th class="header c0 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c0" onclick="naover('c0');" aria-hidden="true" title="Remover coluna"></i> STATUS </th>
+                            <th class="header c1 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c1" onclick="naover('c1');" aria-hidden="true" title="Remover coluna"></i> CREDENCIADO </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c2" onclick="naover('c2');" aria-hidden="true" title="Remover coluna"></i> CNPJ </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c3" onclick="naover('c3');" aria-hidden="true" title="Remover coluna"></i> NF </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c4" onclick="naover('c4');" aria-hidden="true" title="Remover coluna"></i> VALOR NF </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c5" onclick="naover('c5');" aria-hidden="true" title="Remover coluna"></i> INFORMATIVO </th>
+                            <th class="header c3 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c7" onclick="naover('c7');" aria-hidden="true" title="Remover coluna"></i> EMISSAO DA NF </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c8');" aria-hidden="true" title="Remover coluna"></i> DATA RECEBIMENTO </th>
+                            <th class="header c5 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c9" onclick="naover('c9');" aria-hidden="true" title="Remover coluna"></i> DATA ATESTO </th>
+                            <th class="header c6 lastcol text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c10" onclick="naover('c10');" aria-hidden="true" title="Remover coluna"></i> FISCAL </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c11');" aria-hidden="true" title="Remover coluna"></i> DATA LIMITE PAGAMENTO </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c11');" aria-hidden="true" title="Remover coluna"></i> DATA PAGAMENTO </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c12');" aria-hidden="true" title="Remover coluna"></i>LINK SEI ORDEM BANCARIA </th>
+                        </tr>
+                        <?php
+                            foreach ($nota_glosa as $obj) {
+                                $data_limite = strtotime("+30 day", $obj->data_validacao);
+                            ?>
+                            <tbody>
+                                <tr class=" ">
+                                    <td class="cell c2 text-dark"><?= $obj->status ?></td>
+                                    <td class="cell c2 text-dark"><?= $obj->razao_social ?></td>
+                                    <td class="cell c2 text-dark"><?= $obj->cnpj ?></td>
+                                    <td class="cell c2 text-dark"><?= $obj->numero ?></td>
+                                    <td class="cell c2 text-dark"><?= $obj->valor ?></td>
+                                    <td class="cell c0 text-dark"><?= $obj->informativo ?></td>
+                                    <td class="cell c2 text-dark"><?= date('d/m/Y h:i', $obj->data_emissao) ?></td>
+                                    <td class="cell c2 text-dark"><?= date('d/m/Y h:i', $obj->data_validacao) ?></td>
+                                    <td class="cell c2 text-dark"><?= date('d/m/Y h:i', $obj->data_atesto) ?></td>
+                                    <td class="cell c2 text-dark"><?= $obj->nome ?></td>
+                                    <td class="cell c2 text-dark"><?= date('d/m/Y h:i', $data_limite) ?></td>
+                                    <td class="cell c2 text-dark"><?= date('d/m/Y h:i', $obj->data_pagamento) ?></td>
+                                    <td class="cell c3 text-dark"><?= $obj->doc_sei ?></td>
+                                </tr>
+                                <?php
+                            }
+                                ?>
+                                </tbody>
+                                </table>
+                            <?php
+                        } else if ($nota_pagamento && count($nota_pagamento) > 0) {
+                            ?>
+                    <div id="containerNotaGlosa" role="main" class="align-items-center" style="width:100%"><h2 class="text-center">Relatório da Execução: Nota Pagamento</h2><img src="img/iconexcel.png" width="30" height="30" class="d-print-none" id="btnExport" />
+                    <table class="table table-striped" id="registros">
+                        
+                        <tr class="thead-dark">
+                            <th class="header c0 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c0" onclick="naover('c0');" aria-hidden="true" title="Remover coluna"></i> STATUS </th>
+                            <th class="header c1 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c1" onclick="naover('c1');" aria-hidden="true" title="Remover coluna"></i> CREDENCIADO </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c2" onclick="naover('c2');" aria-hidden="true" title="Remover coluna"></i> CNPJ </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c3" onclick="naover('c3');" aria-hidden="true" title="Remover coluna"></i> NF </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c4" onclick="naover('c4');" aria-hidden="true" title="Remover coluna"></i> VALOR NF </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c5" onclick="naover('c5');" aria-hidden="true" title="Remover coluna"></i> INFORMATIVO </th>
+                            <th class="header c2 text-nowrap text-center" style="" scope="col" style="width:50%"><i class="fa fa-minus-square text-white c6" onclick="naover('c6');" aria-hidden="true" title="Remover coluna"></i> COMPETENCIA </th>
+                            <th class="header c3 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c7" onclick="naover('c7');" aria-hidden="true" title="Remover coluna"></i> EMISSAO DA NF </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c8');" aria-hidden="true" title="Remover coluna"></i> DATA RECEBIMENTO </th>
+                            <th class="header c5 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c9" onclick="naover('c9');" aria-hidden="true" title="Remover coluna"></i> DATA ATESTO </th>
+                            <th class="header c6 lastcol text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c10" onclick="naover('c10');" aria-hidden="true" title="Remover coluna"></i> FISCAL </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c11');" aria-hidden="true" title="Remover coluna"></i> DATA LIMITE PAGAMENTO </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c11');" aria-hidden="true" title="Remover coluna"></i> DATA PAGAMENTO </th>
+                            <th class="header c4 text-nowrap text-center" style="" scope="col"><i class="fa fa-minus-square text-white c8" onclick="naover('c12');" aria-hidden="true" title="Remover coluna"></i>LINK SEI ORDEM BANCARIA </th>
+                        </tr>
+                        
+                        <?php
+
+                            foreach ($nota_pagamento as $obj) {
+                                $data_limite = strtotime("+30 day", $obj->data_validacao)
+                                ?>
+                                <tbody>
+                                   <tr class="">
+                                       <td class="cell c2 text-dark " style=""><?= $obj->status ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= $obj->razao_social ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= $obj->cnpj ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= $obj->numero ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= $obj->valor ?></td>
+                                       <td class="cell c0 text-dark " style=""><?= $obj->informativo ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= date('d/m/Y h:i',($obj->data_emissao)) ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= date('d/m/Y h:i', $obj->data_validacao) ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= $obj->nome?></td>
+                                       <td class="cell c2 text-dark " style=""><?= date('d/m/Y h:i', $obj->data_atesto) ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= date('d/m/Y h:i', $obj->data_executado) ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= date('d/m/Y h:i',($data_limite)) ?></td>
+                                       <td class="cell c2 text-dark " style=""><?= date('d/m/Y h:i',($obj->data_pagamento)) ?></td>
+                                       <td class="cell c3 text-dark " style=""><?= $obj->doc_sei ?></td>
+                                       </tr>
+                                   <?php
+                        }
+                    }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>  
+                <?php
+            
+            ?>
+        </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</body>
+
+</html>
