7d75b8cbb3072d0a785450ac50f3ce238ff6ab28
kakabraga <cauebraga96@outlook.com>
2025-03-31 12:12:04 -0300

    update regulação

--- projeto/form_csv.php
+++ projeto/form_csv.php
@@ -1,3 +1,5 @@
+<script>
+</script>
 <form id="form_csv" method="POST" enctype="multipart/form-data">
     <input type="hidden" name="update">
     <div class="container">
@@ -7,9 +9,13 @@
                     <label for="formFile" class="form-label">Anexar Arquivo Excel</label>
                     <input class="form-control" type="file" name="csv" id="csv" accept=".csv" required>
                 </div>
-                <div class="mb-3">
-                    <button type="button" id="envio" class="btn btn-primary btn-sm" disabled>Enviar Arquivo</button>
-                    <button type="button" id="atualiza" class="btn btn-primary btn-sm" disabled>Atualizar Situação SLA</button>
+                <div id="conteudo">
+                    <div id="texto">
+                    </div>
+                </div>
+                <div class="mb-3" id="gif">
+                    <button type="button" id="envio" name="gif" onclick="gifLoading()"
+                        class="btn btn-primary btn-sm">Enviar Arquivo</button>
                 </div>
             </div>
         </div>
@@ -19,8 +25,7 @@
 <script>
     document.getElementById('csv').addEventListener('change', function () {
         const envioButton = document.getElementById('envio');
-        const atualizaButton = document.getElementById('atualiza');
-        envioButton.disabled = !this.files.length; 
+        envioButton.disabled = !this.files.length;
         if (this.files.length > 0) {
             atualizaButton.disabled = false;
         }
@@ -30,11 +35,18 @@
         const form = document.getElementById('form_csv');
         form.action = "salva_csv.php"; // Define o arquivo de processamento para salvar CSV
         form.submit(); // Envia o formulário
-    });
 
-    document.getElementById('atualiza').addEventListener('click', function () {
-        const form = document.getElementById('form_csv');
-        form.action = "processa_prazo_regulacao.php"; // Altera para a lógica de atualizar SLA
-        form.submit(); // Envia o formulário
     });
+    function gifLoading() {
+        var div = document.getElementById("gif");
+
+        div.style.display = 'none';
+        let gif = document.createElement('img')
+        gif.src = './img/loading_sla_regulacao.gif';
+        gif.className = 'rounded mx-auto d-block';
+        document.getElementById('conteudo').appendChild(gif)
+        let carregamento = document.createElement('p')
+        carregamento.id = 'texto';
+        texto.innerText = 'Processando...';
+    }
 </script>
--- projeto/menu_sla.php
+++ projeto/menu_sla.php
@@ -48,7 +48,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link collapsed" href="sla_regulacao_temporaria.php">
-                    <i class="fa fa-hourglass-half"></i>
+                <i class="fa fa-clock"></i>
                     <span>Regulação Temporaria</span>
                 </a>
             </li>
