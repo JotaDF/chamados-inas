c692265eba622a1105adacfb8ae4f78c472ee61f
JotaDF <j.wilson.df@gmail.com>
2025-04-14 15:30:51 -0300

    Update get_etapa_relatorio.php

--- projeto/get_etapa_relatorio.php
+++ projeto/get_etapa_relatorio.php
@@ -28,28 +28,6 @@ foreach ($lista as $obj) {
             <div class="col mb-0 align-top" style="max-width:35%">
                 <span class="text-white"><b><?= ($obj->data_base > 0 ? date('d/m/Y',$obj->data_base) : ' - ') ?></b></span>
             </div>
-            <div class="editar col align-top text-right" style="max-width:15%">
-               <?php
-                if($obj->ordem >= 1 && $obj->ordem < $total_etapas){
-                ?>
-                <a class="text-white" href="muda_ordem_etapa_relatorio.php?op=d&id=<?= $obj->id ?>&ordem=<?= $obj->ordem ?>&tarefa=<?= $id_tarefa ?>"><i class="fa fa-arrow-down" aria-hidden="true"></i></a>&nbsp;
-                <?php
-                } 
-                if($obj->ordem > 1 && $obj->ordem <= $total_etapas){
-                ?>
-                <a class="text-white" href="muda_ordem_etapa_relatorio.php?op=s&id=<?= $obj->id ?>&ordem=<?= $obj->ordem ?>&tarefa=<?= $id_tarefa ?>"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>
-                <?php
-                }
-                ?>
-                &nbsp;&nbsp;&nbsp;
-                <?php
-                if ($obj->excluir) {
-                    ?>
-                   &nbsp;<span class="text-white"><i class="far fa-trash-alt"></i></span>                
-                    <?php
-                }
-                ?>
-            </div>
         </div>
         <?php
         $txt_mostrar = 'show';
