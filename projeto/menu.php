0d1ab377348e17f4f7c873fe9861063c100300be
JotaDF <j.wilson.df@gmail.com>
2025-05-23 11:06:57 -0300

    Update menu.php

--- projeto/menu.php
+++ projeto/menu.php
@@ -69,6 +69,9 @@ foreach ($acessos as $acesso) {
                 $icon_css = "fa fa-signal";
                 $request = "?texto=". $db_usuario->encryptarMensagem("nome=".$usuario_logado->nome."&login=".$usuario_logado->login."&matricula=".$usuario_logado->matricula."&perfil=".$db_usuario->getAcessoUsuario($usuario_logado->id,15));
                 break; 
+            case 16:
+                $icon_css = "fa fa-question-circle";
+                break;                
                                                 
         }
         
