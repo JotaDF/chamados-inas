173da49b211eb56bfa0fe40cb2cc0ca63e4bfccd
kakabraga <cauebraga96@outlook.com>
2025-05-23 11:02:05 -0300

    Create QuestPergunta.php

---
+++ projeto/dto/QuestPergunta.php
@@ -0,0 +1,11 @@
+<?php 
+
+    Class QuestPergunta {
+        public $id;
+        public $titulo;
+        public $pergunta;
+        public $publicada;
+        public $opcional;
+        public $id_quest_escala;
+        public $excluir;
+    }
