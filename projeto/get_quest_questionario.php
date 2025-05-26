b7c8b7d16be3c99cff0cd609a7590dff8f71a23f
JotaDF <j.wilson.df@gmail.com>
2025-05-26 09:58:18 -0300

    Update get_quest_questionario.php

--- projeto/get_quest_questionario.php
+++ projeto/get_quest_questionario.php
@@ -10,12 +10,14 @@
             echo "  <td>".$obj->id."</td>";
             echo "  <td>".$obj->titulo."</td>";
             echo "  <td>".$obj->descricao."</td>";
+            $btn_alterar = "&nbsp;<button class='btn btn-primary btn-sm' type='button' title='Editar' onclick='alterar(".$obj->id.",\"".$obj->titulo."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>";
+            $btn_aplicacoes = "&nbsp;<a class='btn btn-primary btn-sm' type='button' title='Gerenciar Aplicações' href='quest_gerenciar_aplicacoes_questionario.php?id=".$obj->id."'><i class='fa fa-flag'></i></a>";
+            $btn_categorias = "&nbsp;<a class='btn btn-primary btn-sm' type='button' title='Gerenciar Categorias' href='quest_gerenciar_categorias_questionario.php?id=".$obj->id."'><i class='fa fa-tags'></i></a>";
             if($obj->excluir){
-                echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' title='Editar' onclick='alterar(".$obj->id.",\"".$obj->titulo."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' title='Excluir' onclick='excluir(".$obj->id.",\"".$obj->titulo."\")'><i class='far fa-trash-alt'></i></button></td>";
+                echo "  <td align='center'>".$btn_alterar."&nbsp;<button class='btn btn-danger btn-sm' type='button' title='Excluir' onclick='excluir(".$obj->id.",\"".$obj->titulo."\")'><i class='far fa-trash-alt'></i></button></td>";
             } else {
                 echo "  <td align='center'><button class='btn btn-primary btn-sm' type='button' title='Editar' onclick='alterar(".$obj->id.",\"".$obj->titulo."\",\"".$obj->descricao."\")'><i class='fas fa-edit'></i></button>&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button></td>";                
             }
             echo "</tr>";
         }
 
- 
