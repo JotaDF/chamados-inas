83c1829ad33aa562ac81075d1acc6a2d8e2e02eb
JotaDF <j.wilson.df@gmail.com>
2025-05-26 16:33:03 -0300

    perguntas

--- projeto/get_categorias_questionario.php
+++ projeto/get_categorias_questionario.php
@@ -4,8 +4,11 @@
             echo "<tr>";
             echo "  <td>".$obj->id."</td>";
             echo "  <td>".$obj->nome."</td>";
+            $btn_alterar = "&nbsp;<button class='btn btn-primary btn-sm' type='button' title='Editar' onclick='alterar(".$obj->id.",\"".$obj->nome."\",".$questionario->id.")'><i class='fas fa-edit'></i></button>";
+            $btn_excluir = "&nbsp;<button class='btn btn-danger btn-sm' type='button' title='Excluir' onclick='excluir(".$obj->id.",\"".$obj->nome."\",".$questionario->id.")'><i class='far fa-trash-alt'></i></button>";
+            $btn_perguntas = "&nbsp;<a class='btn btn-info btn-sm' type='button' title='Gerenciar Perguntas' href='quest_gerenciar_perguntas_categoria.php?id=".$obj->id."&id_questionario=".$questionario->id."'><i class='fa fa-question'></i></a>";
             if($usuario_logado->perfil < 2){
-                echo "  <td align='center'><button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->nome."\",".$questionario->id.")'><i class='far fa-trash-alt'></i></button></td>";
+                echo "  <td align='center'>".$btn_alterar . $btn_excluir."</td>";
             } else {
                 echo "  <td align='center'> - </td>";                
             }
--- projeto/quest_gerenciar_categorias_questionario.php
+++ projeto/quest_gerenciar_categorias_questionario.php
@@ -80,7 +80,7 @@ and open the template in the editor.
                         ?>
                         <div class="container-fluid">
                             <!-- Exibe dados da  tarefa -->
-                            <div class="card mb-3 border-primary" style="max-width: 800px;">
+                            <div class="card mb-3 border-primary" style="max-width: 1000px;">
                                 <div class="card-body bg-gradient-primary" style="min-height: 5.0rem;">
                                     <div class="row">
                                         <div class="col c2 ml-2">
@@ -132,7 +132,7 @@ and open the template in the editor.
                         ?>
 
 
-                        <div class="card mb-4 border-primary" style="max-width:800px">
+                        <div class="card mb-4 border-primary" style="max-width:1000px">
                             <div class="row ml-0 card-header py-2 bg-gradient-primary" style="width:100%">
                                 <div class="col-sm ml-0" style="max-width:50px;">
                                     <i class="fa fa-tags fa-2x text-white"></i> 
