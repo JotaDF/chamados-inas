a9f69b9599636f786ef704d31b9cb498887e61d1
JotaDF <j.wilson.df@gmail.com>
2025-06-04 15:27:45 -0300

    Update painel_pagamentos_pendentes.php

--- projeto/painel_pagamentos_pendentes.php
+++ projeto/painel_pagamentos_pendentes.php
@@ -276,8 +276,8 @@ and open the template in the editor.
                                                     $out_notas .= "  <td> ".$txt_situacao ." </td>";
                                                     $out_notas .= "</tr>";
                                                 }
-                                                foreach ($notas_glosa as $np) {
-                                                    $vln = str_replace("R$","",$np->valor_deferido);
+                                                foreach ($notas_glosa as $ng) {
+                                                    $vln = str_replace("R$","",$ng->valor_deferido);
                                                     $vln= str_replace(" ","",$vln); 
                                                     $vln= str_replace(".","",$vln);
                                                     $vln= str_replace(",",".",$vln); 
@@ -286,14 +286,14 @@ and open the template in the editor.
                                                     $out_notas .= "  <td>".$p->processo_sei."</td>";
                                                     $out_notas .= "  <td>".$p->cnpj."</td>";
                                                     $out_notas .= "  <td>".$p->razao_social."</td>";
-                                                    $out_notas .= "  <td>".$np->numero."</td>";
-                                                    $out_notas .= "  <td>".$np->valor_deferido."</td>";
-                                                    $out_notas .= "  <td>".$np->exercicio."</td>";
-                                                    $out_notas .= "  <td>".date('d/m/Y', $np->data_emissao)."</td>";
-                                                    $out_notas .= "  <td>".date('d/m/Y', $np->data_validacao)."</td>";
-                                                    $out_notas .= "  <td>".date('d/m/Y', $np->data_atesto)."</td>";
-                                                    $out_notas .= "  <td>".date('d/m/Y', strtotime('+30 days', $np->data_validacao))."</td>";
-                                                    $out_notas .= "  <td><b>".$np->status."</b></td>";
+                                                    $out_notas .= "  <td>".$ng->numero."</td>";
+                                                    $out_notas .= "  <td>".$ng->valor_deferido."</td>";
+                                                    $out_notas .= "  <td>".$ng->exercicio."</td>";
+                                                    $out_notas .= "  <td>".date('d/m/Y', $ng->data_emissao)."</td>";
+                                                    $out_notas .= "  <td>".date('d/m/Y', $ng->data_validacao)."</td>";
+                                                    $out_notas .= "  <td>".date('d/m/Y', $ng->data_atesto)."</td>";
+                                                    $out_notas .= "  <td>".date('d/m/Y', strtotime('+30 days', $ng->data_validacao))."</td>";
+                                                    $out_notas .= "  <td><b>".$ng->status."</b></td>";
                                                     $btn_nt_pagar = " - ";
                                                         if($editar){
                                                             $btn_nt_pagar = "<button title='Pagar nota!' class='btn btn-warning btn-sm' type='button' onclick='pagarNota(".$p->id.",".$ng->id.",\"".$ng->numero."\",\"".$ng->valor_deferido."\",\"".$ng->exercicio."\",".$usuario_logado->id.",1)'><i class='fa fa-credit-card'></i></button>";
@@ -301,7 +301,7 @@ and open the template in the editor.
                                                         $out_notas .= "  <td>".$btn_nt_pagar."</td>";
                                                     $out_notas .= "  <td class='text-danger'> Nota Glosa </td>";
                                                     $hoje = mktime (0, 0, 0, date("m"), date("d"),  date("Y"));
-                                                    $dias = ($hoje - strtotime('+30 days', $np->data_validacao))/(60*60*24);
+                                                    $dias = ($hoje - strtotime('+30 days', $ng->data_validacao))/(60*60*24);
                                                     $out_notas .= "  <td>".$dias."</td>";
                                                     $txt_situacao = "NO PRAZO";
                                                     if($dias > 0){
