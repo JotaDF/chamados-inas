5cb3805e715f632f87ef57f10c9d3e620efdb97c
JotaDF <j.wilson.df@gmail.com>
2025-06-06 10:05:24 -0300

    Create Licitacao.php

---
+++ projeto/dto/Licitacao.php
@@ -0,0 +1,13 @@
+<?php
+  class Licitacao {
+      public $id;
+      public $modalidade;
+      public $certame;
+      public $ano;
+
+      //variaveis de manipulação
+      public $excluir;
+      public $status = true;
+      public $msg;
+
+  }
