e98f4b98cb7e69ec30ae3a866c4ae8eafddaa473
JotaDF <j.wilson.df@gmail.com>
2025-08-26 09:57:33 -0300

    Update save_contrato_prestador.php

--- projeto/save_contrato_prestador.php
+++ projeto/save_contrato_prestador.php
@@ -9,7 +9,7 @@ $contrato = new Contrato();
 $contrato->id  = isset($_REQUEST['id']) ? $_REQUEST['id'] : 0;
 $contrato->prestador  = $_REQUEST['id_prestador'];
 $contrato->numero = $_REQUEST['numero'];
-$contrato->valor = $_REQUEST['valor'];
+$contrato->valor = isset($_REQUEST['valor']) ? $_REQUEST['valor'] : '';
 $contrato->ano = $_REQUEST['ano'];
 $contrato->vigente = isset($_REQUEST['vigente']) ? $_REQUEST['vigente'] : 0;
 $op = isset($_REQUEST['op']) ? $_REQUEST['op'] : 1;
@@ -20,7 +20,9 @@ if ($op == 1) {
     $manterContrato->salvar($contrato);
     $manterContrato->addPasta($caminho);
     header('Location: gerenciar_contratos_prestador.php?id='.$contrato->prestador);
-
+ } if ($op == 2) {
+    $manterContrato->salvar($contrato);
+    header('Location: gerenciar_contratos_prestador.php?id='.$contrato->prestador);
  } else {
     $manterContrato->excluir($contrato->id);
     $manterContrato->delPasta($caminho);
