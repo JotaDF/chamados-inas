5aed38d8973651ee892cce11a5afc81272e60a2a
Cauê Braga <cauebraga.dev@gmail.com>
2025-09-04 10:21:23 -0300

    Update enquetes.php

--- projeto/enquetes.php
+++ projeto/enquetes.php
@@ -2,7 +2,7 @@
 //Ascom
 $mod = 21;
 require_once('./verifica_login.php');
-?> 
+?>
 
 <!DOCTYPE html>
 <!--
@@ -11,146 +11,177 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="">
-        <meta name="author" content="">
 
-        <title>Enquetes</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Custom fonts for this template-->
-        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <title>Chamados - INAS</title>
 
-        <!-- Custom styles for this template-->
-        <link href="css/sb-admin-2.min.css" rel="stylesheet">
-        <link rel="shortcut icon" href="favicon.ico" />
-        <!------ Include the above in your HEAD tag ---------->
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet" />
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="favicon.ico" />
+    <!------ Include the above in your HEAD tag ---------->
 
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/responsive/2.2.3/css/responsive.bootstrap4.min.css">
 
-        <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
-        <script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
-        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
-        <script type="text/javascript" class="init">
-         
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" language="javascript" src="js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" language="javascript"
+        src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" language="javascript"
+        src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
+    <script type="text/javascript" language="javascript"
+        src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
+    <script type="text/javascript" class="init">
+        var quillEditor
+        $(document).ready(function () {
+            $('#enquetes').DataTable();
+            const quillOpcoes = {
+                modules: {
+                    toolbar: [
+                        ['bold', 'italic', 'underline'],
+                        ['link'],
+                        [{ 'align': [] }],
+                        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+                    ],
+                },
+                theme: 'snow',
+            };
 
-            $(document).ready(function () {
-                $('#enquetes').DataTable();
+            quillEditor = new Quill('#editor-descricao', quillOpcoes);
+            document.getElementById('form_enquete').addEventListener('submit', function () {
+                var descricaoHTML = quillEditor.root.innerHTML;
+                console.log(document.querySelector('input[name="descricao"]').value = descricaoHTML);
             });
-            function excluir(id, nome) {
-                $('#delete').attr('href', 'del_enquete.php?id=' + id);
-                $('#nome_excluir').text(nome);
-                $('#confirm').modal({show: true});              
-            }
-            function alterar(id, descricao) {
-                $('#id').val(id);
-                $('#descricao').val(descricao);
-                $('#form_enquete').collapse("show");
-                $('#btn_cadastrar').hide();
-            }
+        });
 
-            function selectByText(select, text) {
-                $(select).find('option:contains("' + text + '")').prop('selected', true);
-            }
+        function excluir(id, nome) {
+            $('#delete').attr('href', 'del_enquete.php?id=' + id);
+            $('#nome_excluir').text(nome);
+            $('#confirm').modal({ show: true });
+        }
+        function alterar(id, descricao) {
+            $('#id').val(id); 
+            quillEditor.root.innerHTML = $('#' + id + '_descricao').val();
+            $('#form_enquete').collapse("show");
+            $('#btn_cadastrar').hide();
+        }
 
+        function selectByText(select, text) {
+            $(select).find('option:contains("' + text + '")').prop('selected', true);
+        }
+        function limpaEditor() {
+           quillEditor.root.innerHTML = ""; 
+        }
 
-        </script>
-        <style>
-            body{
-                font-size: small;
-            }
-        </style>
-    </head>
+    </script>
+    <style>
+        body {
+            font-size: small;
+        }
+    </style>
+</head>
 
-    <body id="page-top">
+<body id="page-top">
 
-        <!-- Page Wrapper -->
-        <div id="wrapper">
-            <?php include './menu_ascom.php'; ?>
-            <!-- Content Wrapper -->
-            <div id="content-wrapper" class="d-flex flex-column">
-                <!-- Main Content -->
-                <div id="content">
-                    <?php include './top_bar.php'; ?>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <?php include './menu_admin.php'; ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <?php include './top_bar.php'; ?>
 
-                    <div class="container-fluid">
-                        <?php include './form_enquete.php'; ?>
-                        <!-- Project Card Example -->
-                        <div class="card mb-4 border-primary" style="max-width:1200px">
-                            <div class="row ml-0 card-header py-2 bg-gradient-primary" style="width:100%">
-                                <div class="col-sm ml-0" style="max-width:50px;">
-                                    <i class="fa fa-question fa-2x text-white"></i> 
-                                </div>
-                                <div class="col mb-0">
-                                    <span style="align:left;" class="h5 m-0 font-weight text-white">Enquetes</span>
-                                </div>
-                                <div class="col text-right" style="max-width:20%">
-                                    <button id="btn_cadastrar" class="btn btn-outline-light btn-sm" type="button" data-toggle="collapse" data-target="#form_enquete" aria-expanded="false" aria-controls="form_enquete">
-                                        <i class="fa fa-plus-circle text-white" aria-hidden="true"></i>
-                                    </button>
-                                </div>
+                <div class="container-fluid">
+                    <?php include './form_enquete.php'; ?>
+                    <!-- Project Card Example -->
+                    <div class="card mb-4 border-primary" style="max-width:1200px">
+                        <div class="row ml-0 card-header py-2 bg-gradient-primary" style="width:100%">
+                            <div class="col-sm ml-0" style="max-width:50px;">
+                                <i class="fa fa-question fa-2x text-white"></i>
                             </div>
-                            <div class="card-body">
-                                <table id="enquetes" class="table-sm table-striped table-bordered dt-responsive" style="width:100%">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">ID</th>
-                                            <th scope="col">Enquete</th>
-                                            <th scope="col">Status</th>
-                                            <th scope="col" style="width:180px;">Opções</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php include './get_enquete.php'; ?>
-                                    </tbody>
-                                </table>
+                            <div class="col mb-0">
+                                <span style="align:left;" class="h5 m-0 font-weight text-white">Enquetes</span>
+                            </div>
+                            <div class="col text-right" style="max-width:20%">
+                                <button id="btn_cadastrar" class="btn btn-outline-light btn-sm" type="button"
+                                    data-toggle="collapse" data-target="#form_enquete" aria-expanded="false"
+                                    aria-controls="form_enquete">
+                                    <i class="fa fa-plus-circle text-white" aria-hidden="true"></i>
+                                </button>
                             </div>
                         </div>
-                    </div>
-                    <!-- End of Main Content -->
-                </div> 
-                <?php include './rodape.php'; ?>
-
-            </div>
-            <!-- End of Content Wrapper -->
-
-        </div>
-        <!-- End of Page Wrapper -->
-
-        <!-- Scroll to Top Button-->
-        <a class="scroll-to-top rounded" href="#page-top">
-            <i class="fas fa-angle-up"></i>
-        </a>
-        <!-- Modal excluir -->
-        <div class="modal fade" id="confirm" role="dialog">
-            <div class="modal-dialog modal-sm">
-
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Confirmação</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Deseja excluir <strong>"<span id="nome_excluir"></span>"</strong>?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <a href="#" type="button" class="btn btn-danger" id="delete">Excluir</a>
-                        <button type="button" data-dismiss="modal" class="btn btn-secondary">Cancelar</button>
+                        <div class="card-body">
+                            <table id="enquetes" class="table-sm table-striped table-bordered dt-responsive"
+                                style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Enquete</th>
+                                        <th scope="col">Status</th>
+                                        <th scope="col" style="width:180px;">Opções</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php include './get_enquete.php'; ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-
+                <!-- End of Main Content -->
             </div>
-        </div>
+            <?php include './rodape.php'; ?>
 
-    </body>
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <!-- Modal excluir -->
+    <div class="modal fade" id="confirm" role="dialog">
+        <div class="modal-dialog modal-sm">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Confirmação</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Deseja excluir <strong>"<span id="nome_excluir"></span>"</strong>?</p>
+                </div>
+                <div class="modal-footer">
+                    <a href="#" type="button" class="btn btn-danger" id="delete">Excluir</a>
+                    <button type="button" data-dismiss="modal" class="btn btn-secondary">Cancelar</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
 
 </html>
