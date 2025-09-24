c0660b39db0661acb787cdd45881f7893a1bc2d7
JotaDF <j.wilson.df@gmail.com>
2025-09-24 17:05:35 -0300

    teste

--- projeto/licitacoes.php
+++ projeto/licitacoes.php
@@ -55,13 +55,15 @@ require_once 'verifica_login.php';
 
             quillObjeto = new Quill('#editor-objeto', { theme: 'snow' });
 
-            document.getElementById('form_licitacao').addEventListener('submit', function () {
-                const descHTML = quillObjeto.root.innerHTML;
-                document.querySelector('input[name="objeto"]').value = descHTML;
-            });
+            //document.getElementById('form_licitacao').addEventListener('submit', function () {
+            //    const descHTML = quillObjeto.root.innerHTML;
+            //    document.querySelector('input[name="objeto"]').value = descHTML;
+            //});
 
         });
-
+        function pegarObjeto() {
+            $('#objeto').val(quillObjeto.root.innerHTML);
+        }
         function alterar(id, modalidade, certame, ano) {
                 $('#modalidade').val(modalidade);
                 $('#certame').val(certame);
