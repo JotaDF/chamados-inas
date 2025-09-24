fc1385c87e6a45307170599c1c9ceac76e93070c
JotaDF <j.wilson.df@gmail.com>
2025-09-24 16:48:00 -0300

    Update licitacoes.php

--- projeto/licitacoes.php
+++ projeto/licitacoes.php
@@ -35,6 +35,7 @@ require_once 'verifica_login.php';
         <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap4.min.js"></script>
 
     <script type="text/javascript" class="init">
+        var quillObjeto;
         $(document).ready(function () {
             $('#licitacoes').DataTable({
                 paging: true // Habilita a paginação
@@ -52,7 +53,7 @@ require_once 'verifica_login.php';
             theme: 'snow',
         };
 
-        const quillObjeto = new Quill('#editor-objeto', quillOpcoes);
+        quillObjeto = new Quill('#editor-objeto', quillOpcoes);
 
         document.getElementById('form_licitacao').addEventListener('submit', function () {
             const descHTML = quillObjeto.root.innerHTML;
