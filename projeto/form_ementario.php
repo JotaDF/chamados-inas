13e8cae78cab15784885dcd547521e8ce9c95c51
JotaDF <j.wilson.df@gmail.com>
2025-10-16 15:25:14 -0300

    Update form_ementario.php

--- projeto/form_ementario.php
+++ projeto/form_ementario.php
@@ -11,21 +11,19 @@
         <form id="form_cadastro" action="save_ementario.php" method="post"> 
             <input type="hidden" id="id" name="id"/>
             <input type="hidden" id="id_usuario" name="id_usuario" value="<?=$usuario_logado->id ?>"/>
-            <div class="form-group row">
-                <div class="input-group">
-                    <label for="processo_sei" class="col c0 col-form-label">Processo SEI:</label>
-                    <input type="text" name="processo_sei" class="col c1 form-control form-control-sm" id="processo_sei" style="width: 120px;" required>
+            <div class="form-row">
+                <div class="form-group col-md-5">
+                <label for="processo_sei">Processo SEI <span class="text-danger font-weight-bold">*</span></label>
+                <input type="text" class="form-control form-control-sm" name="processo_sei" id="processo_sei" required>
                 </div>
-                <div class="input-group">
-                    <label for="doc_sei" class=" col c2 col-form-label">DOC SEI:</label>
-                    <input type="text" name="doc_sei" class="col c3 form-control form-control-sm" id="doc_sei" style="width: 120px;" required>
+                <div class="form-group col-md-3">
+                <label for="doc_sei">DOC SEI <span class="text-danger font-weight-bold">*</span></label>
+                <input type="text" name="doc_sei" class="form-control form-control-sm" id="doc_sei" required>
                 </div>
-            </div>
-            <div class="form-group row">
-                <label for="nota_juridica" class="col-sm-2 col-form-label">Nota Jurídica:</label>
-                <div class="col-sm-10">
-                    <input type="text" name="nota_juridica" class="form-control form-control-sm" id="nota_juridica" required>
-                </div>
+                <div class="form-group col-md-4">
+                <label for="nota_juridica">Nota Jurídica <span class="text-danger font-weight-bold">*</span></label>
+                   <input type="text" name="nota_juridica" class="form-control form-control-sm" id="nota_juridica" required>
+                </div>                
             </div>
             <div class="form-group row">
                 <div class="mb-3 w-100">
