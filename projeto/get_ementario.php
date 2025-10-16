69386164dd426fa5bc6e0f4513fb830fb9ad9af5
JotaDF <j.wilson.df@gmail.com>
2025-10-16 11:23:20 -0300

    Update get_ementario.php

--- projeto/get_ementario.php
+++ projeto/get_ementario.php
@@ -15,7 +15,12 @@
             echo "  <td>".date('d/m/Y h:i', strtotime($obj->atualizado))."</td>";
             $hidden_ementa = "<input type='hidden' id='".$obj->id."_ementa' value='" . $obj->ementa."'>";
             $btn_alterar = $hidden_ementa . "<button class='btn btn-primary btn-sm' type='button' onclick='alterar(".$obj->id.",\"".$obj->processo_sei."\",\"".$obj->doc_sei."\",\"".$obj->nota_juridica."\")'><i class='fas fa-edit'></i></button>";
-            echo "  <td align='center'>" . $btn_alterar . "&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->processo_sei."\")'><i class='far fa-trash-alt'></i></button></td>";
+            $btn_excluir = "";
+            if ($usuario_logado->perfil == 1) {
+                $btn_excluir = "&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id.",\"".$obj->processo_sei."\")'><i class='far fa-trash-alt'></i></button>";
+            }
+            echo "  <td align='center'>" . $btn_alterar . $btn_excluir . "</td>";
+            
             echo "</tr>";
         }
 
