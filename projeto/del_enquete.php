e2d97e8627674e266f54b995303e48dff184538e
JotaDF <j.wilson.df@gmail.com>
2025-10-23 17:33:54 -0300

    Update del_enquete.php

--- projeto/del_enquete.php
+++ projeto/del_enquete.php
@@ -8,6 +8,8 @@ $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : 0;
 
 if ($id > 0) {
     $db_enquete->excluir($id);
+    $caminho = __DIR__.'/enquetes/folder_'. $id .'/';
+    $db_enquete->delPasta($caminho);
     header('Location: enquetes.php');
 } else {
     echo 'Falta de parâmetro!';
