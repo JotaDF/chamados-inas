226fa782ed894d774b707fd63f6144791e32af27
JotaDF <j.wilson.df@gmail.com>
2025-11-12 14:53:48 -0300

    Update testepg.php

--- projeto/testepg.php
+++ projeto/testepg.php
@@ -1,18 +1,18 @@
 <?php 
 
-include_once('actions/ModelPg.php');
+include_once('actions/ManterFilaPericia.php');
 // Conexão
-$db1 = new ModelPg('df_regulacao_consulta_api_live');
-$db2 = new ModelPg('df_contrato_api_live');
-$db3 = new ModelPg('df_pessoa_api_live');
-
+//$db1 = new ModelPg('df_regulacao_consulta_api_live');
+//$db2 = new ModelPg('df_contrato_api_live');
+//$db3 = new ModelPg('df_pessoa_api_live');
+$manterFilaPericia = new ManterFilaPericia('df_regulacao_consulta_api_live');
 // Consultas
-$rs1 = $db1->db->Execute('SELECT * FROM guia WHERE fila_id =18 or id_fila =18');
+$rs1 = $manterFilaPericia->listar();
 print_r($rs1);
 echo "<hr><br/>";
-$rs2 = $db2->db->Execute("SELECT * FROM segurado WHERE uuid = '85b4ad5d-e7fd-4d5d-9c57-40ec929bde81'");
-print_r($rs2);
-echo "<hr><br/>";
-$rs3 = $db3->db->Execute("SELECT * FROM pessoa WHERE cpf_cnpj ='868.303.331-72'");
-print_r($rs3);
-echo "<hr><br/>";
+//$rs2 = $db2->db->Execute("SELECT * FROM segurado WHERE uuid = '85b4ad5d-e7fd-4d5d-9c57-40ec929bde81'");
+//print_r($rs2);
+//echo "<hr><br/>";
+//$rs3 = $db3->db->Execute("SELECT * FROM pessoa WHERE cpf_cnpj ='868.303.331-72'");
+//print_r($rs3);
+//echo "<hr><br/>";
