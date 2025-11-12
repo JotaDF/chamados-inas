1401721a4bece05cc3cc3c726e03be7e6fb29e74
JotaDF <j.wilson.df@gmail.com>
2025-11-12 16:08:52 -0300

    Update ManterFilaPericia.php

--- projeto/actions/ManterFilaPericia.php
+++ projeto/actions/ManterFilaPericia.php
@@ -12,12 +12,17 @@ class ManterFilaPericia extends ModelPg {
         $sql = "SELECT * FROM guia WHERE fila_id =18 or id_fila =18";
         $resultado = $this->db->Execute($sql);
         $array_dados = array();
-        // while ($registro = $resultado->fetchRow()) {
-        //     $dados = array();
-        //     $dados['id'] = $registro["id"];
-        //     $array_dados[] = $dados;
-        // }
-        return $resultado;
+        while ($registro = $resultado->fetchRow()) {
+            $dados = array();
+            $dados['id'] = $registro["id"];
+            $dados['autorizacao'] = $registro["autorizacao"];
+            $dados['data_solicitacao'] = $registro["data_solicitacao"];
+            $dados['id_beneficiario'] = $registro["id_beneficiario"];
+            $dados['justificativa'] = $registro["justificativa"];
+            $dados['situacao'] = $registro["situacao"];
+            $array_dados[] = $dados;
+        }
+        return $array_dados;
     }
 
 }
