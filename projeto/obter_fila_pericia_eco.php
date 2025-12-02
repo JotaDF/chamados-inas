d2e7198152f211e52a9da4baae0ecaa9f6d8fb1f
Cauê Braga <cauebraga.dev@gmail.com>
2025-12-02 17:15:27 -0300

    update

--- projeto/obter_fila_pericia_eco.php
+++ projeto/obter_fila_pericia_eco.php
@@ -1,18 +1,19 @@
 <?php
 header('Content-Type: application/json');
 include('actions/ManterFilaPericiaEco.php');
-
+include('./verifica_login.php');
 $manterFilaPericiaEco = new ManterFilaPericiaEco();
 
 $data_atual = $_GET['data'] ?? date('Y-m-d');
 $periodoDatas = $manterFilaPericiaEco->getPeriodo(new DateTime());
 $periodoHoras = $manterFilaPericiaEco->getHorarios();
 $agenda = $manterFilaPericiaEco->criaAgenda($periodoDatas, $periodoHoras);
+
 $datas = array_keys($agenda);
 if (!$data_atual || !in_array($data_atual, $datas)) {
-    $data_atual = $datas[0];
+    $data_atual = $datas[1];
 }
-$horarios_agendados = $manterFilaPericiaEco->listaHorarioAgendados($data_atual);
+$horarios_agendados = $manterFilaPericiaEco->listaHorarioAgendadosPorData($data_atual);
 if (!is_array($horarios_agendados)) {
     $horarios_agendados = [];
 }
@@ -23,13 +24,16 @@ $index = array_search($data_atual, $datas);
 
 // anterior e próximo
 $anterior = $datas[$index - 1] ?? null;
-$proximo  = $datas[$index + 1] ?? null;
+$proximo = $datas[$index + 1] ?? null;
+
+$dia_semana = date('l', strtotime($data_atual));
 $response = [
     "horarios_disponiveis" => $disponiveis_para_data_atual,
-    "horarios_agendados"   => $horarios_agendados,
-    "data_atual"           => $data_atual,
-    "anterior"             => $anterior,
-    "proximo"              => $proximo
+    "horarios_agendados" => $horarios_agendados,
+    "dia_semana" => $dia_semana,
+    "data_atual" => $data_atual,
+    "anterior" => $anterior,
+    "proximo" => $proximo
 ];
 
 echo json_encode($response);
