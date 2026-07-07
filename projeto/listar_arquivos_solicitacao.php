3bbd5b4d17736d8b4f1bf443e0a06fe81b9af233
JotaDF <j.wilson.df@gmail.com>
2026-07-07 12:11:56 -0300

    Update listar_arquivos_solicitacao.php

--- projeto/listar_arquivos_solicitacao.php
+++ projeto/listar_arquivos_solicitacao.php
@@ -1,9 +1,8 @@
 <?php
-
 header('Content-Type: application/json');
 
 $base = realpath('./anexos_solicitacao');
-$pasta = isset($_POST['pasta']) ? $_POST['pasta'] : '';
+$pasta = $_POST['pasta'] ?? '';
 $caminho_real = realpath($pasta);
 
 if (!$caminho_real || strpos($caminho_real, $base) !== 0 || !is_dir($caminho_real)) {
@@ -11,16 +10,22 @@ if (!$caminho_real || strpos($caminho_real, $base) !== 0 || !is_dir($caminho_rea
     exit;
 }
 
-$arquivos = [];
+$itens = [];
 
 $iterator = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($caminho_real, RecursiveDirectoryIterator::SKIP_DOTS)
+    new RecursiveDirectoryIterator(
+        $caminho_real,
+        RecursiveDirectoryIterator::SKIP_DOTS
+    ),
+    RecursiveIteratorIterator::SELF_FIRST
 );
 
-foreach ($iterator as $arquivo) {
-    if ($arquivo->isFile()) {
-        $arquivos[] = $arquivo->getFilename();
-    }
+foreach ($iterator as $item) {
+    $itens[] = [
+        'tipo' => $item->isDir() ? 'diretorio' : 'arquivo',
+        'nome' => $item->getFilename(),
+        'caminho' => str_replace($base . DIRECTORY_SEPARATOR, '', $item->getPathname())
+    ];
 }
 
-echo json_encode(array_values($arquivos));
+echo json_encode($itens, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
